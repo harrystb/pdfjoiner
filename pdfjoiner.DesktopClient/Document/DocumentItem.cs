60ac51a9d329f1e38ac2b3e6efb40539ecf2f066
Harrison St Baker <harry.stbaker@gmail.com>
2019-10-20 22:34:57 +1000

    User interface initial layout setup. View model has started to be developed, but only contains test items for the moment.

---
+++ pdfjoiner.DesktopClient/Document/DocumentItem.cs
@@ -0,0 +1,56 @@
+﻿using System.IO;
+namespace pdfjoiner
+{
+    public class DocumentItem
+    {
+
+        #region Public Attributes
+        /// <summary>
+        /// The full path of the document
+        /// </summary>
+        public string Path { get; set; }
+        /// <summary>
+        /// The filename of the document.
+        /// </summary>
+        public string Filename { get; set; }
+        /// <summary>
+        /// The number of pages inside the document
+        /// </summary>
+        public int NumberOfPages { get; set; }
+
+        #endregion
+        #region Constructor
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="Path">Full path of the document to be added</param>
+        public DocumentItem(string Path)
+        {
+            this.Path = Path;
+            this.Filename = getFilenameFromPath(Path);
+        }
+        #endregion
+
+        #region Helper Functions
+
+        private string getFilenameFromPath(string path)
+        {
+            if (!File.Exists(path))
+                return string.Empty;
+            string normalisedPath = path.Replace('/', '\\');
+            int  lastIndex = normalisedPath.LastIndexOf('\\');
+            return path.Substring(lastIndex + 1, path.Length - lastIndex - 1);
+        }
+
+           private int getNumberOfPagesFromFile(string path)
+        {
+            if (!File.Exists(path))
+                return 0;
+            //TODO Finish this function. Add in more comments.
+            throw new System.NotImplementedException();
+
+        }
+        #endregion
+
+    }
+}
