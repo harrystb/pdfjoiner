07a20a0b45d27a4ceaa5632c887f05ce6a047749
Harrison St Baker <harry.stbaker@gmail.com>
2022-11-13 22:25:27 +1000

    segment adding working

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 use eframe::egui;
 use eframe::egui::widgets::{Button, Label};
 use eframe::egui::{
-    Align, Color32, Context, Frame, Layout, RichText, Sense, SidePanel, TopBottomPanel, Window,
+    Align, Color32, Context, Frame, Layout, RichText, Sense, SidePanel, Slider, TopBottomPanel,
+    Window,
 };
 use rfd::FileDialog;
 use std::cmp::Ordering;
 use std::collections::HashMap;
+use std::ops::RangeInclusive;
 use std::path::PathBuf;
 
 fn main() {
@@ -19,12 +21,18 @@ fn main() {
 
 struct PdfFile {
     path: PathBuf,
+    title: String,
     data: lopdf::Document,
 }
 
 impl PdfFile {
     fn new(path: PathBuf, data: lopdf::Document) -> Self {
-        Self { path, data }
+        let title = path
+            .file_name()
+            .unwrap_or(path.as_os_str())
+            .to_string_lossy()
+            .to_string();
+        Self { path, title, data }
     }
 }
 
@@ -55,9 +63,14 @@ impl MsgBox {
 
 struct PdfJoinerApp {
     version: String,
-    pdfs: HashMap<String, PdfFile>,
-    selected_pdf: Option<String>,
+    pdfs: HashMap<usize, PdfFile>,
+    current_id: usize,
+    selected_pdf: Option<usize>,
     msg_boxes: Vec<MsgBox>,
+    from_page: usize,
+    to_page: usize,
+    segments: HashMap<usize, (usize, usize, usize)>,
+    current_segment_id: usize,
 }
 
 impl Default for PdfJoinerApp {
@@ -65,8 +78,13 @@ impl Default for PdfJoinerApp {
         PdfJoinerApp {
             version: env!("CARGO_PKG_VERSION").to_owned(),
             pdfs: HashMap::new(),
+            current_id: 0,
             selected_pdf: None,
             msg_boxes: vec![],
+            from_page: 0,
+            to_page: 0,
+            segments: HashMap::new(),
+            current_segment_id: 0,
         }
     }
 }
@@ -80,20 +98,55 @@ impl eframe::App for PdfJoinerApp {
         self.render_right_panel(ctx);
         egui::CentralPanel::default().show(ctx, |ui| {
             ui.vertical(|ui| {
-                ui.heading("Page Selection");
+                ui.heading("2. Select Pages and Add to Generator");
+                ui.separator();
                 match &self.selected_pdf {
                     None => (),
-                    Some(selected_pdf) => {
-                        ui.label(selected_pdf);
-                        match self.pdfs.get_mut(selected_pdf) {
-                            None => {
-                                ui.label(format!("Pdf '{}' no found in list.", selected_pdf));
+                    Some(selected_pdf_id) => match self.pdfs.get_mut(selected_pdf_id) {
+                        None => {
+                            ui.label(format!(
+                                "Pdf with id '{}' not found in list.",
+                                selected_pdf_id
+                            ));
+                        }
+                        Some(pdf_file) => {
+                            ui.label(&pdf_file.title);
+                            let page_count = pdf_file.data.get_pages().len();
+                            ui.label(format!("Page count: {}", page_count));
+                            let resp = ui.add(
+                                Slider::new(
+                                    &mut self.from_page,
+                                    RangeInclusive::new(0, page_count),
+                                )
+                                .text("From"),
+                            );
+                            if resp.clicked() || resp.dragged() {
+                                if self.from_page > self.to_page {
+                                    self.to_page = self.from_page;
+                                }
                             }
-                            Some(pdf_file) => {
-                                ui.label(format!("Pages: {}", pdf_file.data.get_pages().len()));
+                            let resp = ui.add(
+                                Slider::new(&mut self.to_page, RangeInclusive::new(0, page_count))
+                                    .text("To"),
+                            );
+                            if resp.clicked() || resp.dragged() {
+                                if self.to_page < self.from_page {
+                                    self.from_page = self.to_page;
+                                }
+                            }
+                            if ui.button("Add").clicked() {
+                                self.segments.insert(
+                                    self.current_segment_id.to_owned(),
+                                    (
+                                        selected_pdf_id.to_owned(),
+                                        self.from_page.to_owned(),
+                                        self.to_page.to_owned(),
+                                    ),
+                                );
+                                self.current_segment_id += 1;
                             }
                         }
-                    }
+                    },
                 }
             });
         });
@@ -147,24 +200,12 @@ impl PdfJoinerApp {
     fn render_left_panel(&mut self, ctx: &Context) {
         for dropped_file in ctx.input().raw.dropped_files.iter() {
             if let Some(file) = &dropped_file.path {
-                let title = file
-                    .file_name()
-                    .unwrap_or(file.as_os_str())
-                    .to_string_lossy()
-                    .to_string();
-                match lopdf::Document::load(file.as_path()) {
-                    Err(e) => self.msg_boxes.push(MsgBox::new(
-                        "Error while loading pdf".to_owned(),
-                        format!("Could load pdf '{}'.\n{:?}.\nContact Harrison St Baker with this error if you believe this is a valid pdf.", title, e),
-                    )),
-                    Ok(d) => {
-                        self.pdfs.insert(title, PdfFile::new(file.to_owned(), d));
-                    }
-                }
+                self.add_pdf_file(file);
             }
         }
         let resp = SidePanel::left("files").show(ctx, |ui| {
             ui.vertical(|ui| {
+                ui.heading("1. Select Documents");
                 ui.add_space(4.0);
                 if ui
                     .add_sized([ui.available_width(), 20.], Button::new("Add Files"))
@@ -175,32 +216,23 @@ impl PdfJoinerApp {
                         .pick_files()
                         .unwrap_or(vec![]);
                     for file in files {
-                        let title = file
-                            .file_name()
-                            .unwrap_or(file.as_os_str())
-                            .to_string_lossy()
-                            .to_string();
-                        match lopdf::Document::load(file.as_path()) {
-                            Err(e) => self.msg_boxes.push(MsgBox::new(
-                                "Error while loading pdf".to_owned(),
-                                format!("Could load pdf '{}'.\n{:?}.\nContact Harrison St Baker (harry.stbaker@gmail.com) with this error if you believe this is a valid pdf.", title, e),
-                            )),
-                            Ok(d) => {
-                                self.pdfs.insert(title, PdfFile::new(file, d));
-                            }
-                        }
+                        self.add_pdf_file(&file);
                     }
                 }
                 ui.separator();
-                let mut pdfs_names: Vec<String> = self.pdfs.keys().map(|v| v.to_owned()).collect();
-                for pdf in pdfs_names {
+                let mut pdfs_names: Vec<(usize, String)> = self
+                    .pdfs
+                    .iter()
+                    .map(|(id, v)| (id.to_owned(), v.title.to_owned()))
+                    .collect();
+                for (id, pdf_title) in pdfs_names {
                     ui.with_layout(Layout::right_to_left(Align::TOP), |ui| {
                         if ui.add(Button::new("X").small()).clicked() {
-                            self.pdfs.remove(&pdf);
+                            self.pdfs.remove(&id);
                         }
                         let mut is_selected = false;
                         if let Some(selected) = &self.selected_pdf {
-                            if selected == &pdf {
+                            if *selected == id {
                                 is_selected = true;
                             }
                         }
@@ -211,13 +243,19 @@ impl PdfJoinerApp {
                         frame.show(ui, |ui| {
                             ui.with_layout(Layout::left_to_right(Align::TOP), |ui| {
                                 let mut l = match is_selected {
-                                    true => {
-                                        Label::new(RichText::new(&pdf).color(SELECTED_FG_COLOUR))
-                                    }
-                                    false => Label::new(&pdf).sense(Sense::click()),
+                                    true => Label::new(
+                                        RichText::new(format!("{} - {}", id, pdf_title))
+                                            .color(SELECTED_FG_COLOUR),
+                                    ),
+                                    false => Label::new(pdf_title).sense(Sense::click()),
                                 };
                                 if ui.add(l).clicked() {
-                                    self.selected_pdf = Some(pdf);
+                                    self.from_page = 0;
+                                    self.to_page = match self.pdfs.get(&id) {
+                                        None => 0,
+                                        Some(pdffile) => pdffile.data.get_pages().len(),
+                                    };
+                                    self.selected_pdf = Some(id);
                                 }
                             })
                         });
@@ -230,13 +268,60 @@ impl PdfJoinerApp {
     fn render_right_panel(&mut self, ctx: &Context) {
         SidePanel::right("generation").show(ctx, |ui| {
             ui.vertical(|ui| {
+                ui.heading("3. Generation Document");
                 ui.add_space(4.0);
                 if ui
                     .add_sized([ui.available_width(), 20.], Button::new("Generate"))
                     .clicked()
                 {}
                 ui.separator();
+                let segments: Vec<(usize, (usize, usize, usize))> = self
+                    .segments
+                    .iter()
+                    .map(|(seg_id, (id, from, to))| {
+                        (
+                            seg_id.to_owned(),
+                            (id.to_owned(), from.to_owned(), to.to_owned()),
+                        )
+                    })
+                    .collect();
+                for (segment_id, (id, from, to)) in segments {
+                    ui.with_layout(Layout::right_to_left(Align::TOP), |ui| {
+                        if ui.add(Button::new("X").small()).clicked() {
+                            self.segments.remove(&segment_id);
+                        }
+                        match self.pdfs.get(&id) {
+                            None => {
+                                ui.label(
+                                    RichText::new(format!("Id {} no longer found.", id))
+                                        .color(Color32::RED),
+                                );
+                            }
+                            Some(pdffile) => {
+                                ui.with_layout(Layout::left_to_right(Align::TOP), |ui| {
+                                    ui.label(format!(
+                                        "{}\nFrom: {} To: {}",
+                                        pdffile.title, from, to
+                                    ));
+                                });
+                            }
+                        }
+                    });
+                }
             });
         });
+    }
+
+    fn add_pdf_file(&mut self, file: &PathBuf) {
+        match lopdf::Document::load(file.as_path()) {
+            Err(e) => self.msg_boxes.push(MsgBox::new(
+                "Error while loading pdf".to_owned(),
+                format!("Could load pdf '{}'.\n{:?}.\nContact Harrison St Baker (harry.stbaker@gmail.com) with this error if you believe this is a valid pdf.", file.display(), e),
+            )),
+            Ok(d) => {
+                self.pdfs.insert(self.current_id, PdfFile::new(file.to_owned(), d));
+                self.current_id += 1;
+            }
+        }
     }
 }
